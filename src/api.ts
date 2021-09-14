695b2511dca7ea72af64a49b52dd4c216188524f
Kerem Dinçer <b.keremdincer@gmail.com>
2021-09-15 01:43:16 +0300

    Remove `timeout` to handle long response times

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 1000,
   headers: {
     Authorization: process.env.REACT_APP_API_KEY,
   },
