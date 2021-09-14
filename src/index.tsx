50e45f52a35ae72f082f538e1afd6c466e7b9cc2
Kerem Dinçer <b.keremdincer@gmail.com>
2021-09-15 01:41:38 +0300

    Add polyfill for IE11 compatibility (axios)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+require('es6-promise/auto')
 
 ReactDOM.render(
   <React.StrictMode>
